8d0b7b7925973a7e945cec7fe79e6c3f78d8cee5
Ethan1896078 <1896078@qq.com>
2016-11-14 18:08:11 +0800

    created

---
+++ TropicalDiseaseResearch/src/main/java/App.java
@@ -0,0 +1,11 @@
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * desc:
+ * Created by huangzhe on 2016/11/14.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.trim("hello tropical disease research!"));
+    }
+}
