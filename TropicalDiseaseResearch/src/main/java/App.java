dac87c123f2664de81192064fda58796594a73f8
ethanhuang <1896078@qq.com>
2016-11-14 18:57:34 +0800

    check gradle

--- TropicalDiseaseResearch/src/main/java/App.java
+++ TropicalDiseaseResearch/src/main/java/App.java
@@ -1,4 +1,8 @@
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
 
 /**
  * desc:
@@ -7,5 +11,9 @@ import org.apache.commons.lang3.StringUtils;
 public class App {
     public static void main(String[] args) {
         System.out.println(StringUtils.trim("hello tropical disease research!"));
+        List<Integer> list = Lists.newArrayList();
+        list.add(1);
+        list.add(2);
+        System.out.println(JSON.toJSONString(list));
     }
 }
